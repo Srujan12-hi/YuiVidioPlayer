fcd97a5ec379c6d2e17e8910a6090c0584556c6a
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2022-05-03 17:33:18 +0530

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -4,11 +4,11 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**I am A advanced Anime Theme VC Video Player created for playing Video in the voice chats of Telegram Groups & Channels. \n\n**Type /help To View Comands:-** __ \n1) Type /info To View Devs`",   
+       await m.reply(f"**I am A advanced VC Video Player.`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
-                                            "Dev", url="https://t.me/AmiFutami")
+                                            "Dev", url="https://t.me/fake_account_srs")
                                     ]]
                             ))
    else:
